4e4be69ef9fb464acff2c4182e92ffe4ca7417a3
Estevan Gladstone <estevangladstone@ejcm.com.br>
2016-01-03 00:30:34 -0200

    Alterações do Romeu mais condições de segurança e mensagens de erro

---
+++ concorrente.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <math.h>
+#include "testes.h" // Arquivo externo com implementações de funções para testes
+
+/* Protótipos de funções da aplicação */
+float integra(float valor, float l, float r);
+
+/* Variáveis globais */
+float erro; // Erro máximo
+
+float integra(float valor, float l, float r){
+	float m;
+	float f1, f2;
+	float resp; // resultado da integração no intervalo dado
+	float dif; // diferença entre áreas
+
+	m = (r + l)/2; // ponto médio do intervalo
+
+	f1 = funA(l, m);
+	printf("oi 1: %f\n", f1);
+
+	f2 = funA(m, r);
+	printf("oi 2: %f\n", f2);
+	
+	dif = fabsf((f1 + f2) - valor);
+	printf("dif: %f\n", dif);
+
+	if(dif <= erro){
+		resp = valor;
+		printf("LOLOLOLOLOL = %f\n", resp);
+	}
+	else{
+		printf("m-l = %f, r-m = %f\n", m-l, r-m);
+		resp = integra(f1, l, m) + integra(f2, m, r);
+	}
+
+	return resp;
+}
+
+void thread1(){
+	
+}
+
+
+void main(int argc, char *argv[]) {
+	float l, r; // intervalos de integração;
+	float resp; // saída e resultado da integração
+	float valor; // valor da primeira aproximação por retangulo da integral da função
+
+	// valida e recebe os valores de entrada
+	if(argc < 5) {
+		printf("Use: %s <inicio do intervalo de integração> <fim do intervalo de integração> <erro máximo> <número de threads>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	l = atof(argv[1]); // início do intervalo de integração
+	r = atof(argv[2]); // fim do intervalo de integração
+	erro = atof(argv[3]); // erro máximo
+	nThreads = atoi(argv[4]); // número de threads a serem utilizadas
+
+	if((nThreads > 8) || (nThreads < 1)){
+		printf("O número de threads deve ser de 1 a 8!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	valor = funA(l, r); // O valor passado é a base do retângulo
+	printf("oi: %f\n", valor);
+
+	resp = integra(valor, l, r);
+
+	printf("Integral da função no inteval [%f, %f]: %f\n", l, r, area);
+
+	pthread_exit(NULL);
+}
--- testes.h
+++ testes.h
@@ -1,34 +1,36 @@
 /* Cálculo de áreas de funções teste para a aplicação */
 
+/* Calcula a area do retangulo, dependendo da funcao que for passada como parametro. */
+float calculaRet(float (*f)(float), float a, float b){
+    float m = (a+b)/2;
+    return ( (b-a) * f(m) );
+}
+
 /* f(x) = 1 + x */
-float funA(float a, float b){
-	float x = (b + a)/2;
-	return (b - a) * (1 + x);
+float funA(float x){
+	return (1 + x);
 }
 
 /* f(x) = (1 - x^2)^(1/2) , -1 < x < 1 */
-//
-// Ver a parte da condição |x| < 1 !!!!!
-//
-float funB(float a, float b){
-	float x = (b + a)/2;
-	return (b - a) * sqrt(1 - (x * x));
+float funB(float x){
+    if( x < -1 || x > 1 ){
+        printf("OPA, RAIZ DE NUM NEGATIVO, PARSA????????????\n" );
+        exit(EXIT_FAILURE);
+    }
+	return sqrt(1 - (x * x));
 }
 
 /* f(x) = (1 + x^4)^(1/2) */
-float funC(float a, float b){
-	float x = (b + a)/2;
-	return (b - a) * sqrt(1 + pow(x, 4));
+float funC(float x){
+	return sqrt(1 + pow(x, 4));
 }
 
 /* f(x) = sen(x^2) */
-float funD(float a, float b){
-	float x = (b + a)/2;
-	return (b - a) * sin(x * x);
+float funD(float x){
+	return sin(x * x);
 }
 
 /* f(x) = cos(e^(-x)) * (0.005 * x^3 + 1) */
-float funE(float a, float b){
-	float x = (b + a)/2;
-	return (b - a) * (cos(exp(-x)) * ((0.005 * pow(x, 3)) + 1));
+float funE(float x){
+	return ( cos(exp(-x)) * ( ( 0.005 * pow(x, 3) ) + 1 ) );
 }
